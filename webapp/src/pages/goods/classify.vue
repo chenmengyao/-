9b749457e3d6fdb8c4eeb132b0d9d4c9665c3f88
zengjunqin <343946369@qq.com>
2019-05-06 19:07:25 +0800

    更新页面

--- webapp/src/pages/goods/classify.vue
+++ webapp/src/pages/goods/classify.vue
@@ -1,5 +1,77 @@
 <template>
   <div>
-    分类
+    <div>
+      <van-search placeholder="请输入搜索关键词" v-model="keyWord" />
+    </div>
+    <div class="suwis-classify-con">
+      <div class="suwis-classify-btn">
+         <div>手机数码</div>
+         <div>照相机</div>
+         <div>苹果</div>
+         <div>手机数码</div>
+         <div>手机数码</div>
+         <div>手机数码</div>
+         <div>手机数码</div>
+      </div>
+      <div class="suwis-classify-list">
+        <div v-if="classifyBan!=''"><img :src="classifyBan"></div>
+        <div>
+           <div>
+              <span class="d-title">常用分类</span>
+              <div>
+                  <span class="d-tips" v-for="item in 10">安卓</span>
+              </div>
+           </div>
+           <div>
+              <span class="d-title">常用分类</span>
+              <div>
+                  <span class="d-tips" v-for="item in 5">苹果</span>
+              </div>
+           </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return{
+      keyWord:'',//搜索关键词
+      classifyBan:''
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.suwis-classify-con{
+  display: flex;
+  text-align: left;
+}
+.suwis-classify-btn,.suwis-classify-list{
+  flex: 1;
+}
+.suwis-classify-btn{
+  max-width: 100px;
+  min-width: 100px;
+}
+.d-tips{
+  border: 1px solid #ccc;
+  display: inline-block;
+  margin-top:15px;
+  margin-left: 15px;
+  padding:6px 10px;
+  border-radius: 15px;
+}
+.d-title{
+  padding-left:15px;
+  margin-top:15px;
+  display: inline-block
+}
+.suwis-classify-btn div{
+  line-height: 40px;
+  text-align: center;
+  background: #EFEFEF
+}
+</style>
--- webapp/src/pages/news/index.vue
+++ webapp/src/pages/news/index.vue
@@ -1,6 +1,29 @@
 <template lang="html">
-  <div>
-    资讯首页
+  <div> 
+    <div class="suwis-news-ban">
+        <img src="./../../assets/test.jpg">
+    </div>
+    <div>
+      <div class="suwis-news-list">
+         <div class="suwis-news-left">
+            <span>我是标题我是标题我是标题我是标题我是标题我是标题我是标题我是标题我是标题我是标题</span>
+            <p class='suwis-news-tips'><span class='suwis-news-date'>1212</span><span class='suwis-news-num'>1212</span></p>
+         </div>
+         <div class="suwis-news-right">
+           <img src="./../../assets/test.jpg" width="100%">
+         </div>
+      </div>
+      <div class="suwis-news-list">
+         <div class="suwis-news-left">
+            <span>我是标题我是标题我是标题我是标题我是标题我是标题我是标题我是标题我是标题我是标题</span>
+            <p class='suwis-news-tips'><span class='suwis-news-date'>1212</span><span class='suwis-news-num'>1212</span></p>
+         </div>
+         <div class="suwis-news-right">
+           <img src="./../../assets/test.jpg" width="100%">
+         </div>
+      </div>
+      
+    </div>
   </div>
 </template>
 
@@ -9,4 +32,50 @@ export default {}
 </script>
 
 <style lang="css">
+.suwis-news-ban{
+  display: flex;
+  padding: 15px;
+}
+.suwis-news-ban img{
+  flex: 1;
+  width: 100%;
+  height: 100%;
+}
+.suwis-news-list{
+  display: flex;
+  padding: 15px;
+  border-bottom: 1px solid #F5F5F5;
+}
+.suwis-news-left,.suwis-news-right{
+  text-align: left;
+  flex: 1;
+}
+.suwis-news-left{
+  padding-right: 15px
+}
+.suwis-news-right{
+  max-width:80px;
+  height: 80px;
+  border: 1px solid red;
+  border-radius: 5px;
+  overflow: hidden;
+}
+.suwis-news-left>span{
+  font-size: 16px;
+  line-height: 22px;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+}
+.suwis-news-date{
+  float: left
+}
+.suwis-news-num{
+  float: right
+}
+.suwis-news-tips::after{
+  clear:both;
+  content:'';
+}
 </style>
