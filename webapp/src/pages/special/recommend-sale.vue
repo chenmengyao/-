d20433f3b63180be155a9e9bb30f1599f63ba890
zengjunqin <343946369@qq.com>
2019-05-07 13:44:15 +0800

    好物推荐页面

--- webapp/src/pages/special/recommend-sale.vue
+++ webapp/src/pages/special/recommend-sale.vue
@@ -1,6 +1,25 @@
 <template lang="html">
   <div>
-    好物推荐
+     <div class="suwis-recom-head">
+        <div class="suwis-recom-bann">
+            <div class="suwis-recom-banntitle">
+              <span class="d-banntitle-left"><span style="background-image: linear-gradient(to right , #FAA537, #F06B25);">为您挑选人间好物</span></span>
+              <span class="d-banntitle-right">共<span>1314</span>件商品</span>
+            </div>
+            <div style="margin:0 10px;"><img src="./../../../public/test2.png" width="100%"></div>
+        </div>
+     </div>
+     <div class="suwis-recom-list">
+       <div class="suwis-recom-item" v-for="(item,index) in 10">
+         <div :class="index%2==0?'d-flex':'d-flex1'">
+            <div style="flex:1;">
+              <div class="d-recom-img"><img src="./../../../public/test3.png" width="100%"></div>
+              <span class="suwis-recom-title">猫猫包袋女2019新款潮韩 版时尚水桶复古包猫猫包袋女2019新款潮韩 版时尚水桶复古包</span>
+              <span class="d-item-title">推荐指数:</span>
+            </div>
+         </div>
+       </div>
+     </div>
   </div>
 </template>
 
@@ -10,4 +29,84 @@ export default {
 </script>
 
 <style lang="css">
+.suwis-recom-head{
+  background: #F35A5A;
+  display: flex;
+  padding:15px;
+  height: 100px;
+  border-bottom: 50px solid  #fff;
+}
+.suwis-recom-bann{
+  height: 300px;
+  background: #fff;
+  border-radius: 5px;
+  flex: 1;
+}
+.suwis-recom-banntitle::after{
+  content: '';
+  clear: both;
+}
+.d-banntitle-left{
+  text-align: left;
+  float: left;
+  color: #fff;
+}
+.d-banntitle-left span{
+  margin: 10px 0;
+  padding: 0 15px;
+  font-size: 16px;
+  line-height: 31px;
+  display: block;
+  border-top-right-radius: 15px;
+  border-bottom-right-radius: 15px;
+}
+.d-banntitle-right{
+  text-align: right;
+  float:right;
+  padding: 15px;
+  color: #999;
+  font-size: 12px;
+  line-height: 31px;
+}
+.d-banntitle-right span{
+  color:#F06B25
+}
+.suwis-recom-list{
+  display: flex;
+  flex-wrap:wrap;
+  width: 100%;
+  text-align: left;
+}
+.suwis-recom-item{
+  flex: 1;
+  min-width: 50%;
+  max-width: 50%;
+  padding-bottom:15px; 
+}
+.d-item-title{
+  font-size: 12px;
+  color: #666
+}
+.d-recom-img{
+  width: 100%;
+}
+.d-recom-img img{
+  border-radius: 5px
+}
+.suwis-recom-title{
+  font-size: 14px;
+  color: #333;
+  line-height: 20px;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+  margin: 5px 0;
+}
+.d-flex1{
+  display:flex;padding:0 15px 0 7.5px
+}
+.d-flex{
+  display:flex;padding:0 7.5px 0 15px
+}
 </style>
