2bf7bebfee317a9246de02ed701ab444636230c0
ivorzk <ivorzk@qq.com>
2019-05-12 02:27:17 +0800

    v

--- webapp/src/pages/goods/details.vue
+++ webapp/src/pages/goods/details.vue
@@ -1,13 +1,29 @@
 <template lang="html">
-  <div>
-    商品详情
+  <div class="suwis-good-details">
+    <van-row justify="center" class="nav">
+      <van-col span="8">商品</van-col>
+      <van-col span="8">评价</van-col>
+      <van-col span="8">详情</van-col>
+    </van-row>
+    <van-swipe :autoplay="3000" indicator-color="white">
+      <van-swipe-item>
+        <img src="images/index/good_02.jpg" alt="">
+      </van-swipe-item>
+      <van-swipe-item>
+        <img src="images/index/good_02.jpg" alt="">
+      </van-swipe-item>
+    </van-swipe>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
-<style lang="css">
+<style lang="scss" scoped>
+.suwis-good-details {
+    position: relative;
+
+    
+}
 </style>
