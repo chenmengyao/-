8dcf4a952161445df3b62e2436189d442f5c1e2f
ivorzk@qq.com <ivorzk@qq.com>
2019-05-30 16:28:20 +0800

    优惠券相关内容增加！

---
+++ webapp/src/components/coupon-item.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="suwis-coupon-item">
+    
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.suwis-coupon-item {
+    position: relative;
+}
+</style>
--- webapp/src/components/index.js
+++ webapp/src/components/index.js
@@ -3,8 +3,12 @@ import goodList from './good-list'
 import goodItem from './good-item'
 import commentList from './comment-list'
 import commentItem from './comment-item'
+import couponList from './coupon-list'
+import couponItem from './coupon-item'
 // 商品列表
 Vue.component('good-list', goodList)
 Vue.component('good-item', goodItem)
 Vue.component('comment-list', commentList)
 Vue.component('comment-item', commentItem)
+Vue.component('coupon-list', couponList)
+Vue.component('coupon-item', couponItem)
