385f28e71c367840c1a4acc40bb33df2a9e48fce
ivorzk <ivorzk@qq.com>
2019-06-05 01:15:10 +0800

    打包app

--- app/service-worker.js
+++ app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.66cad76b6b2731fc863aaa701411ac59.js"
+  "precache-manifest.88b4f1e685143cf1d83ea6a75a2447a6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "webapp"});
