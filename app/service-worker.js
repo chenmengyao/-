8e2747e507ffb74ff10a51906f2491f70146ed4c
ivorzk@qq.com <ivorzk@qq.com>
2019-06-05 11:53:59 +0800

    更新测试地址！

--- app/service-worker.js
+++ app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.88b4f1e685143cf1d83ea6a75a2447a6.js"
+  "precache-manifest.be6c41eff2b80187e7c7b38438e2b237.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "webapp"});
