e5552901ebba310b24f6023bdd9fc9f7c04338b6
ivorzk <ivorzk@qq.com>
2019-06-09 13:40:14 +0800

    根据type选择！

--- webapp/src/pages/goods/good-status.vue
+++ webapp/src/pages/goods/good-status.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="suwis-good-status" :class="type">
+  <div class="suwis-good-status" v-if="type" :class="type">
     <!-- 底价清仓 -->
     <template v-if="type=='clearance'">
       <dl>
@@ -19,17 +19,50 @@
     <!-- 底价清仓 //-->
     <!-- 竞拍捡漏 -->
     <template v-if="type=='auction'">
-
+      <dl>
+        <dt>
+          竞拍<br>捡漏
+        </dt>
+        <dd>
+          <div class="price">
+            <span>
+              <i>当前价格：</i>
+              <em>¥49.9</em>
+            </span>
+            <var>128次出价</var>
+          </div>
+          <span class="time">
+            <img src="@/assets/details/timer.png" alt="">12:20:00
+          </span>
+        </dd>
+      </dl>
     </template>
     <!-- 竞拍捡漏 //-->
     <!-- 限时抢购 -->
     <template v-if="type=='flash'">
-
+      <dl>
+        <dt>
+          底价<br>清仓
+        </dt>
+        <dd>
+          距离活动开启还剩&nbsp;&nbsp;
+          <i>12</i>:
+          <i>12</i>:
+          <i>01</i>
+        </dd>
+      </dl>
     </template>
     <!-- 限时抢购 //-->
     <!-- 好物推荐 -->
     <template v-if="type=='recommend'">
-
+      <dl>
+        <dt>
+          好物<br>推荐
+        </dt>
+        <dd>
+          推荐指数 <van-rate v-model="test" color="#fff" void-color="#fff" :size="16" />
+        </dd>
+      </dl>
     </template>
     <!-- 好物推荐 //-->
   </div>
@@ -37,7 +70,12 @@
 
 <script>
 export default {
-  props: ['type']
+  props: ['type'],
+  data() {
+    return {
+      test: 4
+    }
+  }
 }
 </script>
 
@@ -97,9 +135,144 @@ export default {
             margin-right: 5px;
         }
     }
+    //
+    &.auction {
+        background: linear-gradient(54deg,rgba(243,90,90,1) 0%,rgba(246,96,62,1) 17%,rgba(221,11,17,1) 100%);
+        dl {
+            margin: 0;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 100%;
 
+            dt {
+                border-right: 1px solid rgba(255,255,255,0.6);
+                padding-right: 15px;
+            }
+
+            dd {
+                display: flex;
+                justify-content: space-between;
+                flex: 1;
+                align-items: center;
+                margin: 0 0 0 10px;
+
+                .price,
+                .time {
+                    display: flex;
+                    align-items: center;
+                    font-size: 14px;
+                }
+
+                .price {
+                    line-height: 16px;
+                    span {
+                        i {
+                            display: block;
+                        }
+                    }
+
+                    var {
+                        font-style: normal;
+                        border: 1px solid #fff;
+                        border-radius: 30px;
+                        padding: 2px 5px;
+                        font-size: 12px;
+                    }
+                }
+            }
+        }
+
+        i {
+            font-size: 12px;
+            font-style: normal;
+            display: inline-block;
+        }
+
+        em {
+            font-size: 16px;
+            font-style: normal;
+        }
+
+        img {
+            width: 22px;
+            display: inline-block;
+            vertical-align: middle;
+            margin-right: 5px;
+        }
+    }
+
+    //
     &.flash {
         background: linear-gradient(220deg,rgba(250,165,55,1) 0%,rgba(247,149,50,1) 28%,rgba(240,107,37,1) 100%);
+        dl {
+            margin: 0;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 100%;
+
+            dt {
+                border-right: 1px solid rgba(255,255,255,0.6);
+                padding-right: 15px;
+            }
+
+            dd {
+                display: flex;
+                flex: 1;
+                align-items: center;
+                margin: 0 0 0 10px;
+                font-size: 14px;
+                white-space: nowrap;
+
+                i {
+                    background: rgba(255,255,255,1);
+                    border-radius: 2px;
+                    font-style: normal;
+                    margin: 0 5px;
+                    width: 21px;
+                    height: 21px;
+                    color: #F9A236;
+                    text-align: center;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    align-content: center;
+                    font-size: 14px;
+                }
+            }
+        }
+    }
+
+    //
+    &.recommend {
+        background: linear-gradient(54deg,rgba(243,90,90,1) 0%,rgba(246,96,62,1) 17%,rgba(221,11,17,1) 100%);
+        dl {
+            margin: 0;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 100%;
+
+            dt {
+                border-right: 1px solid rgba(255,255,255,0.6);
+                padding-right: 15px;
+            }
+
+            dd {
+                display: flex;
+                flex: 1;
+                align-items: center;
+                margin: 0 0 0 10px;
+                font-size: 14px;
+                white-space: nowrap;
+
+                .van-rate {
+                    margin-top: 2.8px;
+                    margin-left: 10px;
+                }
+            }
+        }
     }
 }
 </style>
