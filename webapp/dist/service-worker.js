3b6bce3d7f8ab2096e5bae538041477ead28f3a5
ivorzk@qq.com <ivorzk@qq.com>
2019-06-28 17:36:42 +0800

    修改！

--- webapp/dist/service-worker.js
+++ webapp/dist/service-worker.js
@@ -14,15 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
-<<<<<<< HEAD
-  "precache-manifest.c19788ca07348ac02ad2a74732a42cf2.js"
-=======
-  "precache-manifest.038c15a88a99a5b017e73406d530ecd9.js"
->>>>>>> 6d20d5d60111dc2cf53f5a0e141b6eb32d07f979
-=======
-  "precache-manifest.038c15a88a99a5b017e73406d530ecd9.js"
->>>>>>> 6d20d5d60111dc2cf53f5a0e141b6eb32d07f979
+  "precache-manifest.e7bb3a63598d2e766d8d425183b40628.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "webapp"});
