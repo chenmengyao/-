9c1e5eb78d21105633ed3f261905f4a8f488674e
ivorzk <ivorzk@qq.com>
2019-09-18 21:22:36 +0800

    v

--- webapp/dist/service-worker.js
+++ webapp/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d09b436c506caf98e84e38264eff43e4.js"
+  "precache-manifest.861aa3edf168edb34e9ed90c03059512.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "webapp"});
