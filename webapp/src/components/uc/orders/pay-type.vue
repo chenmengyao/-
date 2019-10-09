554427bfd430a1b186dcef8205aea18bdbe9b892
ivorzk <ivorzk@qq.com>
2019-10-10 00:49:24 +0800

    v

--- webapp/src/components/uc/orders/pay-type.vue
+++ webapp/src/components/uc/orders/pay-type.vue
@@ -173,7 +173,8 @@ export default {
         })
       }, (e) => {
         console.log('----- 支付失败 -----');
-        this.$toast(e.message);
+        let msg = e.message
+        this.$toast(msg.substr(msg.indexOf(']') || 0, msg.length));
         this.$emit('fail', true)
         // plus.nativeUI.alert('更多错误信息请参考支付(Payment)规范文档：http://www.html5plus.org/#specification#/specification/Payment.html', null, '支付失败：' + e.code);
       })
